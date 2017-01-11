85275b27744cfd1e29f2992293bc7101a4b70f27
Ayyayo <ayyayocentral@gmail.com>
2017-01-12 01:30:04 +0530

    feat: notifications

---
+++ docs/render.js
@@ -0,0 +1,6 @@
+/**
+ * Created by manjeshv on 12/01/17.
+ */
+var ban = require('./bangalore.json');
+
+ban.features.forEach(f => console.log(`<p><a href="https://www.ayyayo.com">Buy Water Can Online, ${f.properties.name}, ${f.properties.district}, ${f.properties.state} PINCODE - ${f.properties.pincode} - January 12th 2017 - 20 Ltr Online Water Can - Bisleri, Bailley, Mineral water, Bangalore drinking water home delivery service, Packaged drinking water, 20 Liter Water Can</a></p>`))
